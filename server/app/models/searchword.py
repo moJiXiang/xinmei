df9ae3809eb3b91ec3cc0d44105cf9f371dcb859
moJiXiang <mojixiang1102@gmail.com>
2015-08-28 16:01:36 +0800

    fix bug

--- server/app/models/searchword.py
+++ server/app/models/searchword.py
@@ -3,6 +3,9 @@ from app import db
 from mongoengine import *
 
 class Searchword(Document):
+    meta = {
+        "collection": "searchwords"
+    }
     main = StringField() # 主体：公司或者人
     keyword= StringField() # 关键词
     word= StringField() # 补充关键词团
